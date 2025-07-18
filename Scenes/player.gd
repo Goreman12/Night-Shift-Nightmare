0faf8de0666f06762b0148d43cb48928dcb54fdb
Goreman <jgore5@live.com.au>
2025-07-18 22:51:45 +1000

    Lots of bs

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -8,36 +8,39 @@ var player_damage: int = 25
 
 var acceleration: float = 2.2
 var direction
-var can_attack: bool = true
 var last_moved_direction = "right"
+
+var can_attack: bool = true
 var is_attacking = false
 
+var can_enter_room = false
 
 func _ready() -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	move_player(delta)
 	attack_melee()
+	move_player(delta)
+	
 
 #Currently using flip_h to walk left, should revert this when we have an animation
 func move_player(delta):
 	direction = Input.get_axis("left", "right")
-	if not is_attacking:
+	if velocity.x == 0 and not is_attacking:
+			$AnimatedSprite2D.play("idle")
+	if not is_attacking and velocity.x != 0:
 		if Input.is_action_pressed("right"):
 			last_moved_direction = "right"
-			$AnimatedSprite2D.flip_h = false
-			$AnimatedSprite2D.play("walk")
+			$AnimatedSprite2D.play("walk_right")
 			
-		if Input.is_action_pressed("left"):
+		if Input.is_action_pressed("left") and velocity.x != 0:
 			last_moved_direction = "left"
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.play("walk")
-			
+			$AnimatedSprite2D.play("walk_left")
+		
+		
 			
 	velocity.x = direction * speed * acceleration * delta
 	move_and_slide()
-	
 #Enemies get attacked twice if you attack while moving, unsure why, 
 #maybe add an on_hit function that removes the hitbox, or bring back the 
 #freeze movement effect
@@ -50,18 +53,19 @@ func attack_melee():
 		is_attacking = true
 		if last_moved_direction == "right":
 			collision.position.x = 14.718
-			collision.set_deferred("disabled", 0)
+			collision.set_deferred("disabled", false)
 			$AnimatedSprite2D.play("attack_right")
 			timer.start()
-		elif last_moved_direction == "left":
+		else:
 			collision.position.x = 0.135
-			collision.set_deferred("disabled", 0)
+			collision.set_deferred("disabled", false)
 			$AnimatedSprite2D.play("attack_left")
 			timer.start()
 	if timer.is_stopped() or !$AnimatedSprite2D.is_playing():
-		is_attacking = false
-		collision.set_deferred("disabled", 1)
+		collision.set_deferred("disabled", true)
 		can_attack = true
+		is_attacking = false
+	
 		
 
 
@@ -71,3 +75,13 @@ func _on_melee_area_2d_area_entered(area: Area2D) -> void:
 		current_target.take_damage(player_damage)
 		$MeleeArea2D/MeleeHitBox.set_deferred("disabled", 1)
 		attacked.emit(player_damage)
+
+
+func _on_door_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_parent().is_in_group("Door"):
+		can_enter_room = true
+		print("This is a door!")
+		if Input.is_action_just_pressed("Interact") and can_enter_room:
+			var current_target = area.get_parent() as Node
+			print("Entering room...")
+			current_target.enter_room()
---
+++ node_2d.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@export var connected_room = preload("res://Scenes/door_1.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func enter_room():
+	print("Entering!")
